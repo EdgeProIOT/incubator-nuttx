2c58305e5fa273a77bb981da7a6973d81dd30377
jturnsek <jernej.turnsek@gmail.com>
2022-03-07 11:40:34 +0100

    FlexRAM initialization added

--- arch/arm/src/imxrt/imxrt_start.c
+++ arch/arm/src/imxrt/imxrt_start.c
@@ -149,6 +149,9 @@ void __start(void)
     imxrt_ocram_initialize();
 #endif
 
+  /* Set ITCM, DTCM, OCRAM */
+  imxrt_flexram_initialize();
+
   /* Clear .bss.  We'll do this inline (vs. calling memset) just to be
    * certain that there are no issues with the state of global variables.
    */
