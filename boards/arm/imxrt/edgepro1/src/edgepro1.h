0a3a3fc4e86fa76f674e367c64ed02d1cfcc3d3d
jturnsek <jernej.turnsek@gmail.com>
2022-03-11 10:55:39 +0100

    Working on EdgePro1 board specifics

--- boards/arm/imxrt/edgepro1/src/edgepro1.h
+++ boards/arm/imxrt/edgepro1/src/edgepro1.h
@@ -42,6 +42,42 @@
 
 /* EdgePro1 GPIO Pin Definitions ********************************************/
 
+/* LPSPI1 CS:  GPIO_AD_B0_15 */
+#define IOMUX_LPSPI1_CS       (IOMUX_SLEW_FAST | IOMUX_DRIVE_50OHM | \
+                               IOMUX_SPEED_MEDIUM | IOMUX_PULL_UP_100K | \
+                               _IOMUX_PULL_ENABLE)
+#define GPIO_LPSPI1_CS        (GPIO_OUTPUT | GPIO_OUTPUT_ONE | \
+                               GPIO_PORT1 | GPIO_PIN15 | IOMUX_LPSPI1_CS)
+
+/* LPSPI4 CS:  GPIO_B0_00 */
+#define IOMUX_LPSPI4_CS       (IOMUX_SLEW_FAST | IOMUX_DRIVE_50OHM | \
+                               IOMUX_SPEED_MEDIUM | IOMUX_PULL_UP_100K | \
+                               _IOMUX_PULL_ENABLE)
+#define GPIO_LPSPI4_CS        (GPIO_OUTPUT | GPIO_OUTPUT_ONE | \
+                               GPIO_PORT2 | GPIO_PIN0 | IOMUX_LPSPI4_CS)
+
+/* NINA NRST:  GPIO_AD_B0_00 */
+#define IOMUX_NINA_NRST       (IOMUX_SLEW_FAST | IOMUX_DRIVE_50OHM | \
+                               IOMUX_SPEED_MEDIUM | IOMUX_PULL_UP_100K | \
+                               _IOMUX_PULL_ENABLE)
+#define GPIO_NINA_NRST        (GPIO_OUTPUT | GPIO_OUTPUT_ONE | \
+                               GPIO_PORT1 | GPIO_PIN0 | IOMUX_NINA_NRST)
+
+/* NINA BOOT:  GPIO_AD_B0_02 */
+#define IOMUX_NINA_NRST       (IOMUX_SLEW_FAST | IOMUX_DRIVE_50OHM | \
+                               IOMUX_SPEED_MEDIUM | IOMUX_PULL_UP_100K | \
+                               _IOMUX_PULL_ENABLE)
+#define GPIO_NINA_BOOT        (GPIO_OUTPUT | GPIO_OUTPUT_ONE | \
+                               GPIO_PORT1 | GPIO_PIN2 | IOMUX_NINA_BOOT)
+
+/* NINA GPIO:  GPIO_SD_B1_04 */
+#define IOMUX_NINA_GPIO       (IOMUX_SLEW_FAST | IOMUX_DRIVE_50OHM | \
+                               IOMUX_SPEED_MEDIUM | IOMUX_PULL_UP_100K | \
+                               _IOMUX_PULL_ENABLE)
+#define GPIO_NINA_GPIO        (GPIO_OUTPUT | GPIO_OUTPUT_ONE | \
+                               GPIO_PORT3 | GPIO_PIN4 | IOMUX_NINA_GPIO)
+
+
 /****************************************************************************
  * Public Types
  ****************************************************************************/
@@ -63,6 +99,16 @@
 #if defined(CONFIG_BOARDCTL) || defined(CONFIG_BOARD_LATE_INITIALIZE)
 int imxrt_bringup(void);
 #endif
+
+/****************************************************************************
+ * Name: imxrt_spidev_initialize
+ *
+ * Description:
+ *   Called to configure SPI chip select GPIO pins for the EdgePro1.
+ *
+ ****************************************************************************/
+
+void imxrt_spidev_initialize(void);
 
 /****************************************************************************
  * Name: imxrt_gpio_initialize
